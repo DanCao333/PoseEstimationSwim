9a02fb0c8f4a2d6f2490ccc62f8cbd9912b43014
DanCao333 <155497956+DanCao333@users.noreply.github.com>
2024-01-04 16:49:18 -0800

    Added random things
    Bunch of enters and stuff

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from ai_engine import AIEngine
+import ai_engine
+
 import cv2
 img_grayscale = cv2.imread("IMG_9865.png", 0)
 cv2.imshow("window", img_grayscale)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 cv2.imwrite("IMG-9865.png", img_grayscale)
+
+
+
+
+
+if __name__ == "__main__":
+    pass
+
+
